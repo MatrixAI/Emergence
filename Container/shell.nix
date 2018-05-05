39f3f172dd144be24afd11f4fcd33ab91dbe91f7
Moku <vivian.huang@matrix.ai>
2018-05-05 16:16:58 +1000

    Added container

---
+++ Container/shell.nix
@@ -0,0 +1,14 @@
+{
+  pkgs ? import (fetchTarball https://github.com/NixOS/nixpkgs-channels/archive/7cbf6ca1c84dfc917c1a99524e082fb677501844.tar.gz) {}
+}:
+  with pkgs;
+  haskell.lib.buildStackProject {
+    name = "container-demo";
+    buildInputs = [];
+    shellHook = ''
+      echo 'Building container demo'
+      set -v
+      alias stack="\stack --nix"
+      set +v
+    '';
+  }
