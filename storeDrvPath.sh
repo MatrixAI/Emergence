7ec736faa6d16c8734383315c62ba5056cb69635
Moku <vivian.huang@matrix.ai>
2018-05-09 21:03:43 +1000

    Added notes on store path

---
+++ storeDrvPath.sh
@@ -0,0 +1,17 @@
+drv="/nix/store/y4h73bmrc9ii5bxg6i7ck6hsf5gqv8ck-foo.drv"
+name="foo.sh"
+
+function makePath() {
+	type=$1
+	descr=$2
+	name=$3
+	nix_store="/nix/store"
+
+	echo "$type:sha256:$descr:$nix_store:$name" > foo.str
+
+	echo $(nix-hash --type sha256 --truncate --base32 --flat foo.str)
+}
+
+h=$(nix-hash --type sha256  $drv)
+
+makePath "source" $h $name
