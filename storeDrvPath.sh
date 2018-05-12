f4b10789ee15664591b0abae0a91ec51d6ed92c3
Moku <vivian.huang@matrix.ai>
2018-05-12 19:58:01 +1000

    attempted the final hash again, still isn't working

--- storeDrvPath.sh
+++ storeDrvPath.sh
@@ -6,10 +6,12 @@ function makePath() {
 	descr=$2
 	name=$3
 	nix_store="/nix/store"
-
+	
+	echo -n "$type:$descr:$nix_store:$name" > foo.str
 	echo $(nix-hash --type sha256 --truncate --base32 --flat foo.str)
 }
 
 h=$(nix-hash --type sha256  $drv)
+type="text:/nix/store/xv2iccirbrvklck36f1g7vldn5v58vck-myfile"
 
-makePath "source" $h $name
+makePath $type $h $name
