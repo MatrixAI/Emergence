c810d4dd7cf579f31ea6bf83cd9525a1d4271fdd
Roger Qiu <roger.qiu@polyhack.io>
2018-11-09 20:30:59 +1100

    Initial example of using typed-process

--- cabal.nix
+++ cabal.nix
@@ -1,14 +1,14 @@
-{ mkDerivation, base, hpack, stdenv }:
+{ mkDerivation, base, hpack, stdenv, typed-process }:
 mkDerivation {
   pname = "emergence";
   version = "0.1.0.0";
   src = ./.;
   isLibrary = true;
   isExecutable = true;
-  libraryHaskellDepends = [ base ];
+  libraryHaskellDepends = [ base typed-process ];
   libraryToolDepends = [ hpack ];
-  executableHaskellDepends = [ base ];
-  testHaskellDepends = [ base ];
+  executableHaskellDepends = [ base typed-process ];
+  testHaskellDepends = [ base typed-process ];
   preConfigure = "hpack";
   homepage = "https://github.com/MatrixAI/Emergence#readme";
   license = stdenv.lib.licenses.asl20;
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,15 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Lib
     ( someFunc
     ) where
 
+import System.Process.Typed
+
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
+
+someProc :: IO ()
+someProc = do
+  runProcess "true" >>= print
+  runProcess "false" >>= print
