e05fa29b89679a8dbdf627773518bedd28219dab
Ning Zhang <n.zhang.hp.au@gmail.com>
2018-11-09 22:06:03 +1100

    Added initial typed-process calls to runc

--- cabal.nix
+++ cabal.nix
@@ -1,4 +1,4 @@
-{ mkDerivation, base, hpack, stdenv, typed-process }:
+{ mkDerivation, base, hpack, runc, stdenv, typed-process }:
 mkDerivation {
   pname = "emergence";
   version = "0.1.0.0";
@@ -6,9 +6,11 @@ mkDerivation {
   isLibrary = true;
   isExecutable = true;
   libraryHaskellDepends = [ base typed-process ];
-  libraryToolDepends = [ hpack ];
+  libraryToolDepends = [ hpack runc ];
   executableHaskellDepends = [ base typed-process ];
+  executableToolDepends = [ runc ];
   testHaskellDepends = [ base typed-process ];
+  testToolDepends = [ runc ];
   preConfigure = "hpack";
   homepage = "https://github.com/MatrixAI/Emergence#readme";
   license = stdenv.lib.licenses.asl20;
--- src/Lib.hs
+++ src/Lib.hs
@@ -13,3 +13,25 @@ someProc :: IO ()
 someProc = do
   runProcess "true" >>= print
   runProcess "false" >>= print
+
+data Container = Container { cId :: Int
+                           , cPath :: String
+                           } deriving (Show)
+
+runcCreate :: Container -> IO ()
+runcCreate a = do
+  runProcess (shell $ "cd " ++ (cPath a) ++ " && sudo runc create " ++ (show $ cId a)) >>= print 
+
+runcStart :: Container -> IO ()
+runcStart a = do 
+  runProcess (shell $ "runc start " ++ (show $ cId a)) >>= print
+
+runcKill :: Container -> Maybe String -> IO ()
+runcKill a (Just b) = do
+  runProcess (shell $ "runc kill " ++ (show $ cId a) ++ " " ++ b) >>= print
+runcKill a Nothing = do
+  runProcess (shell $ "runc kill " ++ (show $ cId a)) >>= print
+
+runcDelete :: Container -> IO ()
+runcDelete a = do
+  runProcess (shell $ "runc delete " ++ (show $ cId a)) >>= print
