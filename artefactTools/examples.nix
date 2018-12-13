86b4d45dbcdded6df850f666ab4d510e258ca5d8
Ning Zhang <n.zhang.hp.au@gmail.com>
2018-12-14 08:21:34 +1100

    Added OCI artefact example

--- artefactTools/examples.nix
+++ artefactTools/examples.nix
@@ -1,13 +1,19 @@
 with import <nixpkgs> {};
 let 
-  artefactTools = callPackage ./artefactTools.nix {};
+  artefactTools = callPackage ./. {};
 in 
 rec {
-  bash-artefact = artefactTools.buildNixArtefact {
-    name = "bash-artefact";
+  nix-bash = artefactTools.buildNixArtefact {
+    name = "bash";
     contents = [ bash coreutils ];
     entrypoint = [ "/bin/bash" ];
     terminal = true;
+  };
+
+  oci-busybox = artefactTools.pullOCIArtefact {
+    imageName = "busybox";
+    imageDigest = "sha256:2a03a6059f21e150ae84b0973863609494aad70f0a80eaeb64bddd8d92465812";
+    sha256 = "07q380cdki3v1h4dgwyvjai1y4kpcplgw5x611qi373br8pwafv1";
   };
 }
 
