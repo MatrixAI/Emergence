c48b389466982a2ae8069af3bde11e2741e1ab7b
Ning Zhang <n.zhang.hp.au@gmail.com>
2018-12-15 08:12:11 +1100

    Updated pullOCIArtefact to produce rootfs

--- artefactTools/examples.nix
+++ artefactTools/examples.nix
@@ -5,7 +5,7 @@ in
 rec {
   nix-bash = artefactTools.buildNixArtefact {
     name = "bash";
-    contents = [ bash coreutils ];
+    contents = [ bash tree ];
     entrypoint = [ "/bin/bash" ];
     terminal = true;
   };
@@ -13,7 +13,7 @@ rec {
   oci-busybox = artefactTools.pullOCIArtefact {
     imageName = "busybox";
     imageDigest = "sha256:2a03a6059f21e150ae84b0973863609494aad70f0a80eaeb64bddd8d92465812";
-    sha256 = "07q380cdki3v1h4dgwyvjai1y4kpcplgw5x611qi373br8pwafv1";
+    sha256 = "0cl2rhchj3j7h2bha5bv7crh3xv8zfx5a2dfcccv0m61kpzxadwh";
   };
 }
 
