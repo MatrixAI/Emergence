aca12dcd78095887d4751425618c834280e5407d
Ning Zhang <n.zhang.hp.au@gmail.com>
2018-12-31 17:33:50 +1100

    Added shellout to runc to update resource constraints; Updated type signatures of runc shellout functions; Added pidfile option for runc create and runc run

--- src/Runtime/Runc.hs
+++ src/Runtime/Runc.hs
@@ -1,28 +1,94 @@
+{-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE OverloadedStrings #-}
 
 module Runtime.Runc where
     
+import GHC.Generics
+import Data.Aeson 
+import Data.Word
 import System.Process.Typed
-import Runtime.Types hiding (path)
 
-runcCreate :: Container -> IO ()
-runcCreate (Container cid path) = do
-  runProcess (shell $ "cd " ++ path ++ " && sudo runc create " ++ cid) >>= print 
+runcCreate :: String -> FilePath -> IO ()
+runcCreate name path = 
+  runProcess_ $ proc "sudo" ["runc", "create", "-b", path, "--pid-file", path ++ "/pidFile", name]
+ 
+runcStart :: String -> IO ()
+runcStart name = runProcess_ $ proc "sudo" ["runc", "start", name]
 
-runcStart :: Container -> IO ()
-runcStart (Container cid _) = do 
-  runProcess (shell $ "sudo runc start " ++ cid) >>= print
+runcKill ::  String -> Maybe String -> IO ()
+runcKill name (Just sig) = runProcess_ $ proc "sudo" ["runc", "kill", name, sig]
+runcKill name Nothing = runProcess_ $ proc "sudo" ["runc", "kill", name]
 
-runcKill :: Container -> Maybe String -> IO ()
-runcKill (Container cid _) (Just sig) = do
-  runProcess (shell $ "sudo runc kill " ++ cid ++ " " ++ sig) >>= print
-runcKill (Container cid _) Nothing = do
-  runProcess (shell $ "sudo runc kill " ++ cid) >>= print
+runcDelete ::  String -> IO ()
+runcDelete name = runProcess_ $ proc "sudo" ["runc", "delete", name]
 
-runcDelete :: Container -> IO ()
-runcDelete (Container cid _) = do
-  runProcess (shell $ "sudo runc delete " ++ cid) >>= print
+runcRun ::  String -> FilePath -> IO ()
+runcRun name path = 
+  runProcess_ $ proc "sudo" ["runc","run", "-b", path, "--pid-file", path ++ "/pidFile", name]
 
-runcRun :: Container -> IO ()
-runcRun (Container cid path) = do 
-  runProcess (shell $ "cd " ++ path ++ " && sudo runc run " ++ cid) >>= print 
+data MemoryConstraints = 
+  MemoryConstraints { -- Memory limit (in bytes)
+                      limit :: Maybe Int,
+                      -- Memory reservation or soft_limit (in bytes)
+                      reservation :: Maybe Int,
+                      -- Total memory usage (memory + swap); set '-1' to enable unlimited swap
+                      swap :: Maybe Int,
+                      -- Kernel memory limit (in bytes)
+                      kernel :: Maybe Int,
+                      -- Kernel memory limit (in bytes) for tcp buffer
+                      kernelTcp :: Maybe Int } 
+  deriving (Show, Generic)
+
+instance ToJSON MemoryConstraints where
+  toJSON = genericToJSON defaultOptions { omitNothingFields = True }
+instance FromJSON MemoryConstraints where
+  parseJSON = genericParseJSON defaultOptions { omitNothingFields = True }
+                      
+data CPUConstraints = 
+  CPUConstraints { -- CPU shares (relative weight vs. other containers)
+                   shares :: Maybe Int,
+                   -- CPU CFS hardcap limit (in usecs). Allowed cpu time in a given period
+                   quota :: Maybe Int,
+                   -- CPU CFS period to be used for hardcapping (in usecs). 0 to use system default
+                   period :: Maybe Int,
+                   -- CPU realtime hardcap limit (in usecs). Allowed cpu time in a given period
+                   realtimeRuntime :: Maybe Int,
+                   -- CPU realtime period to be used for hardcapping (in usecs). 0 to use system default
+                   realtimePeriod :: Maybe Int,
+                   -- CPU(s) to use
+                   cpus :: Maybe String,
+                   -- Memory node(s) to use
+                   mems :: Maybe String }
+  deriving (Show, Generic)
+
+instance ToJSON CPUConstraints where
+  toJSON = genericToJSON defaultOptions { omitNothingFields = True }
+instance FromJSON CPUConstraints where
+  parseJSON = genericParseJSON defaultOptions { omitNothingFields = True }
+
+data BlockIOConstraints = 
+  BlockIOConstraints { -- Specifies per cgroup weight, range is from 10 to 1000 (default: 0)
+                     weight :: Maybe Int }
+  deriving (Show, Generic)
+
+instance ToJSON BlockIOConstraints where
+  toJSON = genericToJSON defaultOptions { omitNothingFields = True }
+instance FromJSON BlockIOConstraints where
+  parseJSON = genericParseJSON defaultOptions { omitNothingFields = True }
+
+data ResourceConstraints = 
+  ResourceConstraints { memory :: Maybe MemoryConstraints
+                      , cpu :: Maybe CPUConstraints
+                      , blockIO :: Maybe BlockIOConstraints }
+  deriving (Show, Generic)
+
+instance ToJSON ResourceConstraints where
+  toJSON = genericToJSON defaultOptions { omitNothingFields = True }
+instance FromJSON ResourceConstraints where
+  parseJSON = genericParseJSON defaultOptions { omitNothingFields = True }
+
+runcUpdate :: String -> ResourceConstraints -> IO ()
+runcUpdate name res = runProcess_ $ setStdin (byteStringInput $ encode res) $ proc "sudo" ["runc", "update", "-r", "-", name]
+
+runcList :: IO ()
+runcList = runProcess_ $ proc "sudo" ["runc", "list"]
